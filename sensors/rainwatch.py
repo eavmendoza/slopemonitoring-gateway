7745ffe9f02b9434afafbefb33368cf5abd7c664
earl <earl.mendoza@gmail.com>
2018-07-22 12:07:44 +0800

    Edited rainwatch.py to log to mysql for rain_events. Config tmp file also edited to reflect names

--- sensors/rainwatch.py
+++ sensors/rainwatch.py
@@ -1,20 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 from volmem import client
+from dbio import txn as dbtxn
+from datetime import datetime as dt
 
-def setup():
-    mem = client.get()
-    rain_pin = int(mem.get("cnf")["rain"]["pin"])
+class RainProps:
+    def __init__(self):
+        cnf = client.get().get("cnf")
+        self.rain_pin = int(cnf["rain"]["pin"])
+        self.name = cnf["gateway"]["name"] + cnf["rain"]["name"]    
+
+
+def setup(rg):
+    rain_pin = rg.rain_pin
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(rain_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(rain_pin, GPIO.FALLING, callback=rain_event, bouncetime=300)  
 
-def rain_event(channel):
-    print("hey")
+    cb = lambda channel, arg1=rg: rain_event(channel, arg1)
+    GPIO.add_event_detect(rain_pin, GPIO.FALLING, callback=cb, bouncetime=1000)  
+
+def rain_event(channel, rg):
+    dt_today = dt.today().strftime("%Y-%m-%d %H:%M:%S")
+    dt_today_coded = dt.today().strftime("%y%m%d%H%M%S")
+    print(dt_today, rg.name)
+    query = ("insert into transactions (dt, message) "
+        "values ('%s','rgid:%s;dt:%s')" % (dt_today, rg.name, dt_today_coded))
+
+    dbtxn.write(query)
 
 def main():
-    setup()
+    this_rain_gauge = RainProps()
+    setup(this_rain_gauge)
     try:
         while True:
             time.sleep(10000)
