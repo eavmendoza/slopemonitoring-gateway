e28691f26d665ddb5ab18f333bd4b1c21d2c389b
earl <earl.mendoza@gmail.com>
2018-07-22 10:16:56 +0800

    Directed cnffile.py to read config file in the same directory

--- config/cnffile.py
+++ config/cnffile.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
 from volmem import client
+import os
 
 def set():
+    cfile = os.path.dirname(os.path.realpath(__file__)) + "/this_gateway.cnf"
     mem = client.get()
     config = ConfigParser()
-    config.read('this_gateway.cnf')
+    print(config.read(cfile))
     mem.set("cnf",config)
 
 if __name__ == "__main__":
