34ce6394f710c376ef5e8ed1dc7e2c79e454b872
earl <earl.mendoza@gmail.com>
2018-09-04 23:34:04 +0800

    Added function for inserting items for mqtt publishing

--- volmem/client.py
+++ volmem/client.py
@@ -2,3 +2,9 @@ import memcache
 
 def get():
     return memcache.Client([('127.0.0.1', 11211)])
+
+def push_pub_list(message_value):
+    mc = get()
+    pub_list = mc.get("pub_list")
+    pub_list.append(message_value)
+    mc.set("pub_list", pub_list)
